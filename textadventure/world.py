e77f32047c1c1080cf4d2807099c08a28cd4fb60
nncas <nn.cases@gmail.com>
2015-12-10 17:23:02 +0100

    half fix world

--- textadventure/world.py
+++ textadventure/world.py
@@ -2,13 +2,15 @@ _world = {}
 starting_position = (0, 0)
 
 def load_map():
-    with open('resources/serenity', 'r') as map:
+    with open('resources/serenity.txt', 'r') as map:
         rows = map.readlines()
     x_max = len(rows[0].split('\t'))
+
     for y in range(len(rows)):
-        
-        cols = rows[y].split('\t')
+        cols = rows[y].split()
+
         for x in range(x_max):
+            
             tile_name = cols[x].replace('\n', '')
             if tile_name == 'PilotCabin':
                 global starting_position
