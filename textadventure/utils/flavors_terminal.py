818c5685b6188663c2f4f0e05110046d5494fa5b
nncas <nn.cases@gmail.com>
2016-02-25 11:01:48 +0100

    Changed repository user

---
+++ textadventure/utils/flavors_terminal.py
@@ -0,0 +1,9 @@
+
+
+class Flavors_terminal:
+    ITEM = '\033[92m'
+    WHITE = '\033[0m'
+    MAL =
+    CAPTAIN =
+    ITALIC = '\x1B[3m'
+    ITALIC_END = '\x1B[23m'
---
+++ textadventure/utils/read_text_file.py
@@ -0,0 +1,15 @@
+from flavors_terminal import Flavors_terminal as fla
+
+
+
+def read_firefly(file_path):
+    with open(file_path) as file:
+        lines = file.readlines()
+    for line in lines:
+        if line[0] is 'c':
+            print  + line[4:] + 
+        elif line[0] is 'r':
+            print Colors_terminal.ITEM + line[4:] +Colors_terminal.NORMAL
+
+
+read_firefly('resources/ending.txt')
