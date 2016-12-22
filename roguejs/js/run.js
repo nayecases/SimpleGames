57a3c38a7747624d5d614495e4fc5c9d117d70be
toffeshop <nattanay.90@gmail.com>
2016-12-22 17:43:32 +0100

    key events

--- roguejs/js/run.js
+++ roguejs/js/run.js
@@ -4,7 +4,11 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext('2d');
 var elements = []
 
-
+//-----------------------CONSTANTS---------------
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
 //-----------------------CLASSES-----------------
 
 function Player(){
@@ -12,7 +16,15 @@ function Player(){
   this.y = canvas.height -30;
   this.radius = 10;
   this.color = 'green';
-  this.update = function(){ this.x+=1; this.y+=1}
+  this.dx = 1;
+  this.dy = 1;
+  this.update = function(){
+    if(rightPressed){this.dx=1;this.dy=0;if(checkBoundaries(this)){this.move()};}
+    if(leftPressed){this.dx=-1;this.dy=0;if(checkBoundaries(this)){this.move()};}
+    if(upPressed){this.dx=0;this.dy=-1;if(checkBoundaries(this)){this.move()};}
+    if(downPressed){this.dx=0;this.dy=1;if(checkBoundaries(this)){this.move()};}
+  }
+  this.move = function(){ this.x+=this.dx; this.y+=this.dy}
   this.draw = function(){
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
@@ -26,6 +38,47 @@ function Player(){
 //----------------------INSTANCES-----------------
 
 elements.push(new Player())
+
+//-----------------------FUNCTIONS----------------
+function checkBoundaries(elem){
+  if(elem.x+elem.dx>=canvas.width-elem.radius ||
+    elem.x-elem.radius+elem.dx<0 ||
+    elem.y-elem.radius-elem.dy<0 ||
+    elem.y+elem.dy>=canvas.height-elem.radius){
+    return false
+  }
+  return true
+}
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+    else if(e.keyCode == 38) {
+        upPressed = true;
+    }
+    else if(e.keyCode == 40) {
+        downPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+    else if(e.keyCode == 38) {
+        upPressed = false;
+    }
+    else if(e.keyCode == 40) {
+        downPressed = false;
+    }
+}
 
 
 //-----------------------LIFE CICLE---------------
@@ -43,9 +96,11 @@ function draw(){
   }
 }
 
-function cicle(){
+function cycle(){
   update()
   draw()
 }
 
-setInterval(cicle,1000)
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+setInterval(cycle,10)
